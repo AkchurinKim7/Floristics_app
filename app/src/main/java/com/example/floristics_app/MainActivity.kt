3161f7a53f8859e0539bf5bb0d45c7ea4ab03ad8
Ainzerhald <89225940713@mail.ru>
2020-11-29 23:33:45 +0300

    Рабочий прототип

--- app/src/main/java/com/example/floristics_app/MainActivity.kt
+++ app/src/main/java/com/example/floristics_app/MainActivity.kt
@@ -1,29 +1,35 @@
 package com.example.floristics_app
 
+import android.Manifest
 import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.SQLException
 import android.database.sqlite.SQLiteDatabase
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.prototype.DatabaseHelper
 import com.google.zxing.integration.android.IntentIntegrator
+import net.glxn.qrgen.core.scheme.VCard
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 
-
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     var code = ""
     private var mDb: SQLiteDatabase? = null
     private var mDBHelper: DatabaseHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val scanBtn = findViewById<Button>(R.id.scanBtn)
-        scanBtn.setOnClickListener(this)
+        setContentView(R.layout.main)
 
         mDBHelper = DatabaseHelper(this)
         try {
@@ -36,9 +42,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         } catch (mSQLException: SQLException) {
             throw mSQLException
         }
+        //Check for storage permission
+
+        if (!checkPermissionForExternalStorage()) {
+            requestPermissionForExternalStorage()
+        }
     }
 
-    override fun onClick(v: View?) {
+    fun qrScan(view: View) {
         scanCode()
     }
 
@@ -213,5 +224,94 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         else{
             Toast.makeText(this, "Пустое поле", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    //Function for Generating QR code
+    var vCard = VCard("")
+    var qrImage = net.glxn.qrgen.android.QRCode.from(vCard).bitmap()
+    fun generateQRCode(view: View)
+    {
+        if(findViewById<LinearLayout>(R.id.layout_vCard).visibility == View.VISIBLE)
+        {
+            vCard = VCard(findViewById<EditText>(R.id.input_name).text.toString())
+            /*.setEmail(input_email.text.toString())
+            .setAddress(input_address.text.toString())
+            .setPhoneNumber(input_phoneNumber.text.toString())
+            .setWebsite(input_website.text.toString())*/
+            qrImage =
+                    net.glxn.qrgen.android.QRCode.from(vCard).bitmap()
+            if(qrImage != null)
+            {
+                findViewById<ImageView>(R.id.imageView_qrCode).setImageBitmap(qrImage)
+            }
+        }
+        /*else if(input_text.visibility == View.VISIBLE)
+        {
+            qrImage = net.glxn.qrgen.android.QRCode.from(input_text.text.toString()).bitmap()
+            if(qrImage != null)
+            {
+                imageView_qrCode.setImageBitmap(qrImage)
+                btn_save.visibility = View.VISIBLE
+            }
+        }*/
+    }
+    //function for requesting storage access
+    fun requestPermissionForExternalStorage() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "External Storage permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).show()
+        }
+    }
+    //fuunction for checking storage permission
+    fun checkPermissionForExternalStorage(): Boolean {
+        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    //funtion for saving image into gallery
+    fun saveImage(view: View){
+        var savedImagePath: String? = null
+        var imageFileName = "QR" + getTimeStamp() + ".jpg"
+        var storageDir = File(
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES), "/QRGenerator")
+        var success = true
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs()
+        }
+        if (success) {
+            var imageFile = File(storageDir, imageFileName)
+            savedImagePath = imageFile.getAbsolutePath()
+            try {
+                var fOut = FileOutputStream(imageFile)
+                qrImage.compress(Bitmap.CompressFormat.JPEG, 100, fOut)
+                fOut.close()
+            } catch (e: java.lang.Exception) {
+                e.printStackTrace()
+            }
+
+            var mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+            var f = File(savedImagePath)
+            var contentUri = Uri.fromFile(f)
+            mediaScanIntent.setData(contentUri)
+            sendBroadcast(mediaScanIntent)
+            Toast.makeText(this,"QR Image saved into folder: QRGenerator in Gallery",Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this,"ERROR SAVING IMAGE",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    fun getTimeStamp(): String? {
+        val tsLong = System.currentTimeMillis() / 1000
+        val ts = tsLong.toString()
+
+        return ts
+    }
+
+    fun gen(view: View){
+        setContentView(R.layout.activity_main)
     }
 }
